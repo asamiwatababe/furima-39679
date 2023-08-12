0b489f3e7b1251d6f8a1e2d3a04bce60d2ab59c3
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-08-12 12:30:29 +0900

    ルートパスへアクセスするとダウンロードしたビュー素材が表示された。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'items#index'
+  resources :items, only: :index
+  get 'items', to: 'items#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
 end
