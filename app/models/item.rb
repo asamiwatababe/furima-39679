62ff9239b9dcde3c1e97627a2deed79817e015b6
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-08-15 21:59:10 +0900

    コメントアウト

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,14 @@
 class Item < ApplicationRecord
 
-    belongs_to :user
-    has_one :order
+    #belongs_to :user
+    #has_one :order
 
-    validates :user,        presence: true
-    validates :description, presence: true
-    validates :item_status, presence: true
-    validates :name,       presence: true
-    validates :shipping_charge_id, presence: true
-    validates :prefecture, presence: true
-    validates :price, presence: true
-    validates :day_to_ship_id, presence: true
+    #validates :user,        presence: true
+    #validates :description, presence: true
+    #validates :item_status, presence: true
+    #validates :name,       presence: true
+    #validates :shipping_charge_id, presence: true
+    #validates :prefecture, presence: true
+    #validates :price, presence: true
+    #validates :day_to_ship_id, presence: true
 end
