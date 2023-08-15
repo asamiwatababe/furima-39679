e056288ee76bb16700ff2584bc4243711954ef86
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-08-15 10:36:40 +0900

    user.rbにてバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, null: false
+  validates :first_name, null: false
+  validates :last_name, null: false
+  validates :first_name_kana, null: false
+  validates :last_name_kana, null: false
+  validates :birth_day, null: false
 end
