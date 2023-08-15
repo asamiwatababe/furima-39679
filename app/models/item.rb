b7f6a710cbab704ae9ca240dcf6cede133d35c7f
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-08-15 10:46:02 +0900

    item,rbでバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+    validates :user,        null: false, foreign_key: true
+    validates :description, null: false
+    validates :item_status, null: false
+    validates :name,       null: false
+    validates :shipping_charge_id, null: false
+    validates :prefecture, null: false
+    validates :price, null: false
+    validates :day_to_ship_id, :null_false
 end
