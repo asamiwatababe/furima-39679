186a36f9abc472f08c76dce59f9afe2fc3c3c5d0
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-08-16 08:37:56 +0900

    user.rbでバリデーション追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,10 @@ class User < ApplicationRecord
   #has_many :orders
 
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角ひらがな、漢字、カタカナで入力してください" }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角ひらがな、漢字、カタカナで入力してください" }
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{Katakana}\p{Blank}ー－]+\z/, message: "は全角カタカナで入力してください" }
+  validates :last_name_kana, presence: true, format: { with: /\A[\p{Katakana}\p{Blank}ー－]+\z/, message: "は全角カタカナで入力してください" }
   validates :birth_date, presence: true
+  validates :password, presence: true, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: "は半角英字と数字を組み合わせて入力してください" }
 end
