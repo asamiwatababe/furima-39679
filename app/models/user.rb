608aaa99dd101742ff272063e7d876f910fdb3cc
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-08-16 14:31:28 +0900

    コメントアウト削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
   ## パスワードの半角英数字混合入力必須
   ## 氏名それぞれ全角ひらがな漢字カタカナ入力必須
   ## 氏名かなそれぞれ全角カタカナ入力必須
+  
 
   validates :nickname, presence: true
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角ひらがな、漢字、カタカナで入力してください" }
@@ -17,5 +18,5 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true, format: { with: /\A[\p{Katakana}\p{Blank}ー－]+\z/, message: "は全角カタカナで入力してください" }
   validates :last_name_kana, presence: true, format: { with: /\A[\p{Katakana}\p{Blank}ー－]+\z/, message: "は全角カタカナで入力してください" }
   validates :birth_date, presence: true
-  validates :password, presence: true, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: "は半角英字と数字を組み合わせて入力してください" }
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: "は半角英字と数字を組み合わせて入力してください" }
 end
