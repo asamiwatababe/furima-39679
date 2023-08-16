5c6ee940c51403bf129eee7a97d6eaee34e2c724
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-08-16 16:09:07 +0900

    passwordカラムを修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
     factory :user do
       nickname              {Faker::Name.initials(number: 2)}
       email                 {Faker::Internet.email}
-      password              {Faker::Internet.password(min_length: 6)}
+      password              {Faker::Internet.password(min_length: 6)+ 'A1'}
       password_confirmation {password}
     end
   end
