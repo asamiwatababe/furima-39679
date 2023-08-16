791164b3d1917485246f13099ea41e8abb86e57d
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-08-16 10:53:19 +0900

    バリデーション追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,13 @@ class User < ApplicationRecord
   #has_many :items
   #has_many :orders
 
+  ## パスワードの半角英数字混合入力必須
+  ## 氏名それぞれ全角ひらがな漢字カタカナ入力必須
+  ## 氏名かなそれぞれ全角カタカナ入力必須
+
   validates :nickname, presence: true
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角ひらがな、漢字、カタカナで入力してください" }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角ひらがな、漢字、カタカナで入力してください" }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角ひらがな、漢字、カタカナで入力してください" }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角ひらがな、漢字、カタカナで入力してください" }
   validates :first_name_kana, presence: true, format: { with: /\A[\p{Katakana}\p{Blank}ー－]+\z/, message: "は全角カタカナで入力してください" }
   validates :last_name_kana, presence: true, format: { with: /\A[\p{Katakana}\p{Blank}ー－]+\z/, message: "は全角カタカナで入力してください" }
   validates :birth_date, presence: true
