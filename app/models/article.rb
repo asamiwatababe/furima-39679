92b71bf52762f02ef9372e4a6bea9b455a5a6fd3
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-08-17 18:34:21 +0900

    Articleモデルのアソシエーション

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :day_to_ship
+  belongs_to :item_status
+  belongs_to :prefecture
+  belongs_to :shipping_charge
 end
