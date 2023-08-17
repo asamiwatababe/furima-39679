5f4f10dc171ee9afbec5f2bbe97dde5fa4fb7373
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-08-17 18:42:57 +0900

    can't be blankをエラーメッセージに追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,10 @@ class Article < ApplicationRecord
   belongs_to :item_status
   belongs_to :prefecture
   belongs_to :shipping_charge
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :day_to_ship_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_charge_id, numericality: { other_than: 1, message: "can't be blank" }
 end
