9c054bc0cc9c72ade4ce59cbabba6b11048ad2b0
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-08-18 08:19:22 +0900

    バリデーション追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,12 +9,12 @@ class Item < ApplicationRecord
 
 
 
-    #validates :user,        presence: true
-    #validates :description, presence: true
-    #validates :item_status, presence: true
-    #validates :name,       presence: true
-    #validates :shipping_charge_id, presence: true
-    #validates :prefecture, presence: true
-    #validates :price, presence: true
-    #validates :day_to_ship_id, presence: true
+    validates :user,        presence: true
+    validates :description, presence: true
+    validates :item_status, presence: true
+    validates :name,       presence: true
+    validates :shipping_charge_id, presence: true
+    validates :prefecture, presence: true
+    validates :price, presence: true
+    validates :day_to_ship_id, presence: true
 end
