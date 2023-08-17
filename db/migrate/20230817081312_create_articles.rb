7f659b735305dee00e51c43a6e63a34b2d8050f5
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-08-17 18:31:45 +0900

    アソシエーション設定

--- db/migrate/20230817081312_create_articles.rb
+++ db/migrate/20230817081312_create_articles.rb
@@ -1,6 +1,11 @@
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
+      t.integer :category, null: false
+      t.integer :item_status, null: false
+      t.integer :shipping_charge, null: false
+      t.integer :prefecture, null: false
+      t.integer :day_to_ship, null: false
 
       t.timestamps
     end
