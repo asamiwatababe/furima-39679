01e18299aa16082626dbc806e22d2eca8d09433f
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-08-18 09:45:37 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,17 +4,26 @@ class Item < ApplicationRecord
     #has_one :order
     has_one_attached :image
     
+    with_options numericality: { other_than: 1, message: "can't be blank" } do
+      validates :category_id
+      validates :item_status_id
+
+
+
+    end
 
 
 
 
 
     validates :user,        presence: true
+    validates :item_status_id, presence: true
+    validates :category_id, presence: true
     validates :description, presence: true
-    validates :item_status, presence: true
+    validates :item_status_id, presence: true
     validates :name,       presence: true
     validates :shipping_charge_id, presence: true
-    validates :prefecture, presence: true
+    validates :prefecture_id, presence: true
     validates :price, presence: true
     validates :day_to_ship_id, presence: true
 end
