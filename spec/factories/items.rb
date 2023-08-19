a8ac41c97a855d3ec6e4f0949524083d297191c9
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-08-19 20:19:23 +0900

    テストコード　修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,21 +1,18 @@
 FactoryBot.define do
   factory :item do
-    name                  { '服' }
-    description           { 'まだ使えます' }
-    category_id           { 2 }
-    item_status_id        { 2 }
-    shipping_charge_id    { 2 }
-    prefecture_id         { 2 }
-    day_to_ship_id        { 2 }
-    price                 { 50000 }
+    name                  { Faker::Commerce.product_name }
+    description           { Faker::Lorem.sentence }
+    category_id           { Faker::Number.between(from: 1, to: 10) }
+    item_status_id        { Faker::Number.between(from: 1, to: 5) }
+    shipping_charge_id    { Faker::Number.between(from: 1, to: 3) }
+    prefecture_id         { Faker::Number.between(from: 1, to: 47) }
+    day_to_ship_id        { Faker::Number.between(from: 1, to: 3) }
+    price                 { Faker::Number.between(from: 100, to: 100000) }
 
     association :user
 
-      after(:build) do |item|
-        item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
-      end
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
+  end
 end
-  
-  
-  
