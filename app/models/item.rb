8b718365d95c0feb5d6b3791f9f1594ed064d5f6
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-08-19 20:34:31 +0900

    userのバリデーション削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
       validates :day_to_ship_id
     end
 
-    validates :user,        presence: true
+    
     validates :category_id, presence: true
     validates :description, presence: true
     validates :item_status_id, presence: true
