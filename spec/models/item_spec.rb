cfd3b27cd65b9c00bf9474f9ad7a88e7c869d73e
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-08-19 21:03:09 +0900

    価格の異常系テストコード追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -61,6 +61,24 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Price can't be blank")
     end
 
+    it '価格に半角数字以外が含まれている場合は出品できない' do
+      @item.price = '1a2b3c'  # 数字以外を含む価格を設定
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is not a number")
+    end
+
+    it '価格が300円未満では出品できない' do
+      @item.price = 299  # 300未満の価格を設定
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+    end
+
+    it '価格が9,999,999円を超えると出品できない' do
+      @item.price = 10_000_000  # 9,999,999を超える価格を設定
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+    end
+
     it '発送日が空では登録できない' do
       @item.day_to_ship = 1
       @item.valid?
