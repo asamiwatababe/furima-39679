cff209fbfed48d1335a23eb9fffba9636514d9c3
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-08-22 14:26:53 +0900

    showアクションに対して未ログイン時の制限を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   #before_action :set_item,except: [:index, :new, :create]
-  before_action :authenticate_user!, only: [:new, :create]
-
+  before_action :authenticate_user!, only: [:new, :create, :show]# ログインしていない場合、showアクションを制限
+  
     def index 
       @items = Item.all.order(created_at: :desc)  # 作成日時の新しい順に並べ替え  
     end
