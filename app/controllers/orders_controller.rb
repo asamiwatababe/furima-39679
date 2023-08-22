f8e6b445d1e3b965b56a50505eb4715a4471caf0
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-08-22 18:15:25 +0900

    create アクション記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,4 +3,15 @@ class OrdersController < ApplicationController
     @item = Iten.find(params[:item_id])
     @orderform = OrderForm.new
   end
+
+  def create 
+    @orderform = OrderForm.new(order_params)
+    if @orderform.valid?
+       @orderform.save(params,current_user.id)
+       redirect_to root_path
+    else
+      @item = Iten.find(params[:item_id])
+      render :index
+    end
+  end
 end
