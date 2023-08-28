9c6c2e9093a996a98762df5e2b41b65fcf35d68f
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-08-28 13:44:16 +0900

    コンソール内のエラー修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,47 +1,33 @@
+window.addEventListener('load', function(){
 const pay = () => {
-  const publicKey = gon.public_key;
-  const payjp = Payjp(publicKey); // PAY.JPテスト公開鍵
-  const elements = payjp.elements();
-  const numberElement = elements.create('cardNumber');
-  const expiryElement = elements.create('cardExpiry');
-  const cvcElement = elements.create('cardCvc');
+    const publicKey = gon.public_key;
+    const payjp = Payjp(publicKey); // PAY.JPテスト公開鍵
+    const elements = payjp.elements();
+    const numberElement = elements.create('cardNumber');
+    const expiryElement = elements.create('cardExpiry');
+    const cvcElement = elements.create('cardCvc');
 
-  numberElement.mount('#number-form');
-  expiryElement.mount('#expiry-form');
-  cvcElement.mount('#cvc-form');
+    numberElement.mount('#number-form');
+    expiryElement.mount('#expiry-form');
+    cvcElement.mount('#cvc-form');
 
-  const form = document.getElementById('charge-form');
-  form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-
-    const submitButton = form.querySelector('input[type="submit"]');
-    submitButton.disabled = true; // 送信ボタンを無効化
-
-    const response = await payjp.createToken(numberElement);
-    
-    if (response.error) {
-      // エラーハンドリング: エラーメッセージの表示や必要な処理を追加
-      const errorDisplay = document.getElementById('error-message');
-      errorDisplay.textContent = response.error.message;
-
-      // エラーが発生した場合、送信ボタンを再度有効化する
-      submitButton.disabled = false;
-    } else {
-      const token = response.id;
-      const renderDom = document.getElementById('charge-form');
-      const tokenObj = `<input value=${token} name='token' type="hidden">`;
-      renderDom.insertAdjacentHTML('beforeend', tokenObj);
-
-      // トークンをサーバーに送信する代わりに、ここで別のカスタム処理を実行可能
-
-      // サブミット
-      document.getElementById('charge-form').submit();
-    }
-
-    numberElement.clear();
-    expiryElement.clear();
-    cvcElement.clear();
-  });
-};
-
-window.addEventListener('turbo:load', pay);
+    const form = document.getElementById('charge-form')
+    form.addEventListener("submit", (e) => {
+        payjp.createToken(numberElement).then(function (response) {
+            if (response.error) {
+            } else {
+              const token = response.id;
+              const renderDom = document.getElementById("charge-form");
+              const tokenObj = `<input value=${token} name='token' type="hidden">`;
+              renderDom.insertAdjacentHTML("beforeend", tokenObj);
+            }
+            numberElement.clear();
+            expiryElement.clear();
+            cvcElement.clear();
+            document.getElementById("charge-form").submit();
+          });
+      e.preventDefault();
+    });
+}});
+  
+//window.addEventListener("turbo:load", pay)/
